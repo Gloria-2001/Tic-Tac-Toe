ae92b100b32e9c5c30d3adea5d068ef373fbc5b6
Zait06 <zait-hdez@hotmail.com>
2021-01-07 18:03:14 -0600

    Botones

--- client/Client.java
+++ client/Client.java
@@ -1,38 +1,88 @@
 import java.io.*;
 import java.net.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class Client{
+public class Client extends JFrame implements ActionListener{
 
-    private String host;
+    private String host,name;
     private int port;
     private Socket sc;
     private BufferedReader msgIn;
     private PrintWriter msgOut;
     public Scanner scan = new Scanner(System.in);
+    private Container cp;
+    private GridLayout gl;
+    private Label l00;
 
     public Client(){
+        super("Tic Tac Toe");
+        this.setSize(450,450);
         port = 1234;
         host = "localhost";
     }
 
     public Client(int p){
+        super("Tic Tac Toe");
+        this.setSize(450,450);
         port = p;
         host = "localhost";
     }
 
     public Client(String h, int p){
-        host = h;
+        super("Tic Tac Toe");
+        this.setSize(450,450);
         port = p;
+        host = h;
     }
 
     public void init(){
         try {
             sc = new Socket(host,port); //Socket para el cliente
+            this.cp = getContentPane();
+            this.gl = new GridLayout(3,3);
+            gl.setHgap(5);  gl.setVgap(5);
+            name = JOptionPane.showInputDialog(null, "Ingrese su nombre", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            this.initGUI();
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(0);
         }
+    }
+
+    public void initGUI(){
+        crearBarra();
+        crearBotones();
+    }
+
+    public void crearBarra(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(new Label(name));
+        l00 = new Label("Espera tu turno");
+        panel.add(l00);
+        cp.add(panel,BorderLayout.NORTH);
+    }
+
+    public void crearBotones(){
+        JPanel panel = new JPanel();
+        panel.setLayout(this.gl);
+        // this.no.setActionCommand("Cancelar");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                JButton b = new JButton("");
+                b.setActionCommand(i+","+j);
+                b.addActionListener(this);
+                panel.add(b);
+            }
+        }
+        // for(int i=1; i<=9; i++){
+        //     panel.add(new JButton(String.valueOf(i)));
+        // }
+        cp.add(panel,BorderLayout.CENTER);
     }
 
     public void runClient() throws IOException{
@@ -50,23 +100,30 @@ public class Client{
         }
 
         String linea;
+        this.setVisible(true);
 
         try {
             while(go){
                 linea = msgIn.readLine();
+                System.out.println(linea);
                 switch (linea) {
                     case "exit":
                         go = false;
                     break;
                     case "name":
                         // Ingresa un nombre
-                        linea = scan.nextLine();
-                        msgOut.println(linea);
+                        msgOut.println(name);
                     break;
                     case "doMark":
                         // Da coordeandas para tirar
                         linea = scan.nextLine();
                         msgOut.println(linea);
+                    break;
+                    case "w00":
+                        JOptionPane.showMessageDialog(null, "Experando a otro jugador", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                    case "play":
+                        // this.setVisible(true);
                     break;
                     default:
                         // Respuesta del servidor
@@ -81,6 +138,12 @@ public class Client{
         // Libera recursos
         msgOut.close();
         msgIn.close();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        // e.getSource() se compara con la instancia de la clase
+        System.out.println(e.getActionCommand());
     }
 
     public void close() throws IOException{
